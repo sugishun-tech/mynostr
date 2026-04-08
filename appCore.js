5c4ebc3d4f27794d9427e3570240f0e81fe0d5e4
Shun Sugiyama <shun.sugiyama.tech@proton.me>
2026-04-08 13:36:31 +0900

    update

--- appCore.js
+++ appCore.js
@@ -14,10 +14,10 @@ export const app = {
   currentThreadId: null,
   
   state: {
-    public:  { newest: 0, oldest: Math.floor(Date.now()/1000) },
-    home:    { newest: 0, oldest: Math.floor(Date.now()/1000) },
-    profile: { newest: 0, oldest: Math.floor(Date.now()/1000) },
-    notifications: { newest: 0, oldest: Math.floor(Date.now()/1000) },
-    thread:  { newest: 0, oldest: Math.floor(Date.now()/1000) }
+    public:  { newest: Math.floor(Date.now()/1000), oldest: Math.floor(Date.now()/1000) },
+    home:    { newest: Math.floor(Date.now()/1000), oldest: Math.floor(Date.now()/1000) },
+    profile: { newest: Math.floor(Date.now()/1000), oldest: Math.floor(Date.now()/1000) },
+    notifications: { newest: Math.floor(Date.now()/1000), oldest: Math.floor(Date.now()/1000) },
+    thread:  { newest: Math.floor(Date.now()/1000), oldest: Math.floor(Date.now()/1000) }
   }
 };
